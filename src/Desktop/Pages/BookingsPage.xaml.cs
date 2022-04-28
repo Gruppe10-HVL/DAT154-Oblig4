624a2c74b3a7252a6dafc66c8d3507443058cd17
andersbuanes <anderstbuanes@hotmail.com>
2022-04-28 19:38:14 +0200

    Add filters for customer, room and status on bookings

--- src/Desktop/Pages/BookingsPage.xaml.cs
+++ src/Desktop/Pages/BookingsPage.xaml.cs
@@ -26,6 +26,10 @@ namespace Desktop.Pages
     /// </summary>
     public sealed partial class BookingsPage : Page
     {
+        const string Url = "https://localhost:5001/api/v1/booking";
+        List<int> CustomerIds = new List<int>();
+        List<int> RoomIds = new List<int>();
+        List<BookingStatus> Statuses = new List<BookingStatus>();
         List<BookingDto> Bookings = new List<BookingDto>();
         List<BookingDto> BookingsQuery = new List<BookingDto>();
         public BookingsPage()
@@ -40,11 +44,17 @@ namespace Desktop.Pages
             HttpClient httpClient = new HttpClient(clientHandler);
             httpClient.Timeout = TimeSpan.FromSeconds(30);
 
-            var api = "https://localhost:5001/api/v1/booking";
+            var api = Url;
             var response = await httpClient.GetStringAsync(api);
             var bookings = JsonConvert.DeserializeObject<List<BookingDto>>(response);
             Bookings = bookings;
+            //CustomerIds = Bookings.Select(x => x.CustomerName).Distinct().ToList();
+            RoomIds = Bookings.Select(x => x.RoomId).Distinct().ToList();
+            Statuses = Bookings.Select(x => x.Status).Distinct().ToList();
             BookingsMenu.ItemsSource = Bookings;
+            CustomerCombo.ItemsSource = CustomerIds;
+            RoomCombo.ItemsSource = RoomIds;
+            BookingsStatusCombo.ItemsSource = Statuses;
         }
 
         private void SearchCustomerButton_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
@@ -76,12 +86,26 @@ namespace Desktop.Pages
         {
             BookingDto booking = (BookingDto)BookingsMenu.SelectedItem;
 
+            if (booking.BookingStart > DateTime.Now)
+            {
+                ContentDialog invalidCheckinDialog = new ContentDialog
+                {
+                    Title = "Invalid checkin",
+                    Content = "Booking has not started yet.",
+                    CloseButtonText = "Ok"
+                };
+
+                await invalidCheckinDialog.ShowAsync();
+
+                return;
+            }
+
             HttpClientHandler clientHandler = new HttpClientHandler();
             HttpClient httpClient = new HttpClient(clientHandler);
             httpClient.Timeout = TimeSpan.FromSeconds(30);
 
-            var api = "https://localhost:5001/api/v1/booking/checkin";
-            var request = new HttpRequestMessage(new HttpMethod("PATCH"), api + "/?id=" + booking.Id);
+            var api = Url + "/checkin/?id=";
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), api + booking.Id);
             var response = await httpClient.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
@@ -111,8 +135,8 @@ namespace Desktop.Pages
             HttpClient httpClient = new HttpClient(clientHandler);
             httpClient.Timeout = TimeSpan.FromSeconds(30);
 
-            var api = "https://localhost:5001/api/v1/booking/checkout";
-            var request = new HttpRequestMessage(new HttpMethod("PATCH"), api + "/?id=" + booking.Id);
+            var api = Url + "/checkout/?id=";
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), api + booking.Id);
             var response = await httpClient.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
@@ -130,6 +154,7 @@ namespace Desktop.Pages
 
                 if (result == ContentDialogResult.Primary)
                 {
+                    // TODO: Create service task
                     Console.WriteLine("Here");
                 } else
                 {
@@ -139,6 +164,114 @@ namespace Desktop.Pages
                 GetBookings();
             }
                 
+        }
+
+        private async void DeleteBookingButton_Click(object sender, RoutedEventArgs e)
+        {
+            BookingDto booking = (BookingDto)BookingsMenu.SelectedItem;
+            if (booking == null)
+            {
+                ContentDialog invalidDeleteOptionDialog = new ContentDialog
+                {
+                    Title = "No booking selected",
+                    Content = "Select a booking to delete.",
+                    CloseButtonText = "Ok"
+                };
+
+                await invalidDeleteOptionDialog.ShowAsync();
+
+                return;
+            }
+
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            HttpClient httpClient = new HttpClient(clientHandler);
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
+
+            var api = Url + "/cancel/?id=";
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), api + booking.Id);
+            var response = await httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+                GetBookings();
+        }
+
+        private void CreateBookingButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BookingPage));
+        }
+
+        private void RoomCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            if (combo.SelectedItem == null) return;
+
+            var id = combo.SelectedItem;
+            List<BookingDto> bookings = new List<BookingDto>();
+
+            bookings = Bookings.Where(x => x.RoomId == (int)id).ToList();
+            BookingsMenu.ItemsSource = bookings;
+
+        }
+
+        private void RoomComboClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            RoomCombo.SelectedItem = null;
+            BookingsMenu.ItemsSource = Bookings;
+        }
+
+        private void CustomerCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            if (combo.SelectedItem == null) return;
+
+            var id = combo.SelectedItem;
+            List<BookingDto> bookings = new List<BookingDto>();
+
+            bookings = Bookings.Where(x => x.CustomerName == (string)id).ToList();
+            BookingsMenu.ItemsSource = bookings;
+        }
+
+        private void CustomerComboClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerCombo.SelectedItem = null;
+            BookingsMenu.ItemsSource = Bookings;
+        }
+
+        private void BookingsStatusCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            if (combo.SelectedItem == null)
+            {
+                return;
+            }
+
+            var status = combo.SelectedItem;
+            List<BookingDto> bookings = new List<BookingDto>();
+
+            bookings = Bookings.Where(x => x.Status == (BookingStatus)status).ToList();
+            BookingsMenu.ItemsSource = bookings;
+        }
+
+        private void BookingsStatusComboClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            BookingsStatusCombo.SelectedItem = null;
+            BookingsMenu.ItemsSource = Bookings;
+        }
+    }
+
+    public class NullItemToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }

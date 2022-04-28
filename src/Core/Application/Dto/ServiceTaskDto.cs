cb518c1cedaf31e923d43fa4fc50c270e71321b2
Rune Mæstad <rune.maestad@gmail.com>
2022-04-28 11:59:39 +0200

    Added JsonPropertyName attributes to ServiceTaskDto

--- src/Core/Application/Dto/ServiceTaskDto.cs
+++ src/Core/Application/Dto/ServiceTaskDto.cs
@@ -1,16 +1,30 @@
-﻿using DAT154Oblig4.Domain.Enums;
+﻿using System.Text.Json.Serialization;
+using DAT154Oblig4.Domain.Enums;
 
 
 namespace DAT154Oblig4.Application.Dto
 {
     public class ServiceTaskDto
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        
+        [JsonPropertyName("roomId")]
         public int RoomId { get; set; }
+        
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+        
+        [JsonPropertyName("taskType")]
         public ServiceTaskType TaskType { get; set; }
+        
+        [JsonPropertyName("taskStatus")]
         public ServiceTaskStatus TaskStatus { get; set; }
+        
+        [JsonPropertyName("priority")]
         public ServiceTaskPriority Priority { get; set; }
+        
+        [JsonPropertyName("notes")]
         public string Notes { get; set; }
 
     }

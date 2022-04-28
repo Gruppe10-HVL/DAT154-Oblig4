f2666154053058ef24e30742f7c8aa20209db9e1
andersbuanes <anderstbuanes@hotmail.com>
2022-04-28 15:14:10 +0200

    Add search by customer name for bookings

--- src/Desktop/Pages/BookingsPage.xaml.cs
+++ src/Desktop/Pages/BookingsPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Desktop.Entities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,9 +25,51 @@ namespace Desktop.Pages
     /// </summary>
     public sealed partial class BookingsPage : Page
     {
+
+        List<BookingDto> Bookings = new List<BookingDto>();
+        List<BookingDto> BookingsQuery = new List<BookingDto>();
         public BookingsPage()
         {
             this.InitializeComponent();
+            GetBookings();
+        }
+
+        async void GetBookings()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            HttpClient httpClient = new HttpClient(clientHandler);
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
+
+            var api = "https://localhost:5001/api/v1/booking";
+            var response = await httpClient.GetStringAsync(api);
+            var bookings = JsonConvert.DeserializeObject<List<BookingDto>>(response);
+            Bookings = bookings;
+            BookingsMenu.ItemsSource = Bookings;
+        }
+
+        private void SearchCustomerButton_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
+        {
+            var query = args.QueryText;
+            var bookings = Bookings.Where(x => x.CustomerName.Contains(query)).ToList();
+            Bookings = bookings;
+            BookingsMenu.ItemsSource = BookingsQuery;
+        }
+
+        private void SearchCustomerButton_QueryChanged(SearchBox sender, SearchBoxQueryChangedEventArgs args)
+        {
+
+            var query = args.QueryText;
+
+            if (query == String.Empty)
+            {
+                BookingsMenu.ItemsSource = Bookings;
+            } else
+            {
+                var bookings = Bookings.Where(x => x.CustomerName.Contains(query)).ToList();
+                Bookings = bookings;
+                BookingsMenu.ItemsSource = BookingsQuery;
+            }
+
         }
     }
 }

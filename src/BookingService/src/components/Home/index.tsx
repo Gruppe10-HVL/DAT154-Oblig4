0b9f482f6b21f69f4135ea2c9473d8070cc49f9c
Markus Bjermeland <markus.bjermeland@zdata.no>
2022-04-29 09:58:51 +0200

    remove test code

--- src/BookingService/src/components/Home/index.tsx
+++ src/BookingService/src/components/Home/index.tsx
@@ -175,7 +175,7 @@ export const Home = () => {
                 return (
                   <div key={room.id} className="card bg-dark w-50">
                     <div className="card-body">
-                      <h5 className="card-title">Room {room.id} (fjern)</h5>
+                      <h5 className="card-title">Room</h5>
                       <h6 className="card-subtitle mb-2 text-muted">
                         {getRoomQuality(room.quality)} Quality
                       </h6>
